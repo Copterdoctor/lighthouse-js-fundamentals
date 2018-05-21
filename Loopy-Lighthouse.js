db90b5702e3e3c7119c249078ecbbbca20c03f3a
Copterdoctor <jordananders@me.com>
2018-05-21 15:51:36 +0000

    Add Loopys

--- Loopy-Lighthouse.js
+++ Loopy-Lighthouse.js
@@ -2,7 +2,7 @@ for (var i = 100; i < 201; i++) {
   if (i % 3 === 0 && i % 4 === 0) {
     console.log("LoopyLighthouse");
   } else if (i % 3 === 0) {
-    console.log("Loopy")
+    console.log("Loopy");
   } else if (i % 4 === 0) {
     console.log("Lighthouse");
   } else {
